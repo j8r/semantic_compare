6b9bb35ace0c0e3689ce25c39b2295f07ea7d73d
Julien Reichardt <mi@jrei.ch>
2017-10-13 22:51:36 +0200

    Fix to two examples in spec

--- spec/semantic_compare_spec.cr
+++ spec/semantic_compare_spec.cr
@@ -7,7 +7,7 @@ describe SemanticCompare do
         SemanticCompare.version("1.2.3", expr).should be_true
       end
     end
-    describe "complex comparisons with 'or' `||`" do
+    it "complex comparisons with 'or' `||`" do
       [ "1.2.3 || <=2.0.0", ">=1.0.4 || <=1.2.3", ">=1.2.0 || <1.4.0 || ~1.2.5", ">0.0.1 || 1.2.0 - 1.3.0" ].each do |expr|
         SemanticCompare.expression("1.2.3", expr).should be_true
       end
